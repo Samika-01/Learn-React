c5bd6e8d27eaa49357c8bcaa7d82d27367c1820c
Samika-01 <samikataja01@gmail.com>
2025-09-08 12:10:45 +0545

    learn component in react

---
+++ component/App.jsx
@@ -0,0 +1,20 @@
+import Component from "./Component";
+import Button from "./Button";
+import Heading from "./Heading";
+
+function App() {
+  return(
+    <div>
+      <h1>Learn components in React. </h1>
+      <Heading></Heading>
+    <Component></Component>
+    <Button></Button>
+
+    </div>
+    
+  );
+
+
+}
+
+export default App;
---
+++ component/Component.jsx
@@ -0,0 +1,8 @@
+
+function Component(){
+    return(
+        <h1>Welcome to the website</h1>
+    );
+}
+
+export default Component;
